bb36526ca9104f05450931fb58bc45f9e0f923ab
SamioneX <47018118+SamioneX@users.noreply.github.com>
2019-06-21 00:01:56 -0500

    Update priorityqueue.h

--- priorityqueue.h
+++ priorityqueue.h
@@ -4,7 +4,7 @@
 #include "binaryheap.h"
 
 namespace my {
-    template <class T, class C = my::greater<T> > class priority_queue: public my::binary_heap<T,C> {
+    template <class T, class C = my::less<T> > class priority_queue: public my::binary_heap<T,C> {
         public:
         priority_queue(const C& comp = C()): my::binary_heap<T,C>(comp) {}
         template <class InputIterator>
